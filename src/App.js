d578a857754daa916f9c2e967120d716f9f607c7
Josh Bello <Jb230913@live.com>
2022-06-08 21:47:43 +0100

    setup routes and pages for project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { storeProducts } from './product';
+import { useState } from 'react';
 
 function App() {
+
+ // console.log(storeProducts);
+
   return (
+    <>
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +26,9 @@ function App() {
         </a>
       </header>
     </div>
+
+    </>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Cart from './Cart';
+import Checkout from './Checkout';
 import reportWebVitals from './reportWebVitals';
 
+import { Route,Routes,BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<App/>}/>
+      <Route exact path="/cart" element={<Cart/>}/>
+      <Route exact path="/checkout" element={<Checkout/>}/>
+    </Routes>
+</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
